ff1927148f7b1e18d1b95ee3052f0996d747eded
dborzenkov <danielspb@gmail.com>
2021-01-09 20:44:59 -0500

    Adding POJO's. closing #2

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinic1Application.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinic1Application.java
@@ -1,4 +1,4 @@
-package guru.springfarmework.sfgpetclinic1;
+package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
